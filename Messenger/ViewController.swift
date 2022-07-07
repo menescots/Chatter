1d6459655f78c43baec533d2d9b9e1cbd44d7841
menescots <agatamenes@gmail.com>
2022-07-07 14:06:28 +0200

    initial commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Agata Menes on 07/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
